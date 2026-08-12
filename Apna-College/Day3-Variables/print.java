180afb1143d7b3be63d63a26031ad33abd4ff933
Samrudh B N <samrudhbn12@gmail.com>
2026-08-12 18:57:06 +0530

    day3 variables,input and output

---
+++ Apna-College/Day3-Variables/print.java
@@ -0,0 +1,13 @@
+// Question 1 - print the patter *
+// **
+// ***
+// ****
+public class print {
+  public static void main(String[] args) {
+    // Code to print the pattern
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+  }
+}
---
+++ Apna-College/Day3-Variables/variables.java
@@ -0,0 +1,12 @@
+
+public class variables {
+   public static void main(String[]  args){
+    int a = 10;
+    int b = 10;
+    int sum = a+b;
+    System.out.println(sum);
+    int mul = a*b;
+    System.out.print(mul);
+    // basic operations are perfomed here for example.
+   } 
+}
