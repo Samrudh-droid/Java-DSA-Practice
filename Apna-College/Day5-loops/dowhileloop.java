675f026920f682e2f74a0a98052e2dc8cc1c4b83
Samrudh B N <samrudhbn12@gmail.com>
2026-08-15 14:39:57 +0530

    day5 - loops covered for,do, and while loops

---
+++ Apna-College/Day5-loops/dowhileloop.java
@@ -0,0 +1,9 @@
+public class dowhileloop {
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.println("Hello Loops in Java");
+            i++;
+        }while(i<=10);
+    }
+}
